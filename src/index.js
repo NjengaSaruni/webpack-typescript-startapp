e0828b838c9063436116da2df6eb5aec9c9ae61d
Peter Njenga <njengasaruni@gmail.com>
2019-09-26 22:32:27 +0300

    Loading images

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+import './style.css';
+import Image from './image.jpg';
+
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = 'Hello webpack'
+    element.classList.add('hello');
+
+    // Add an image to the existing div
+    const myImage = new Image();
+    myImage.src = Image;
+
+    element.appendChild(myImage);
 
     return element;
 }
